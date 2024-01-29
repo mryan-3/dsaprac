8f1f2a1639f1d724f2d03a9cf0ea4a71bb60b20b
mryan-3 <malawaryan@gmail.com>
2024-01-29 22:58:17 +0300

    Checking if a number has multiple values of the same type

---
+++ arraysC/repeat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//Checking if a number has multiple same values
+int main(){
+    int seen[10] = {0};
+    int N;
+    printf("Enter the number: \n");
+    scanf("%d", &N);
+
+    int rem;
+
+    while (N > 0) {
+        rem = N%10;
+        if (seen[rem] == 1)
+            break;
+        seen[rem] = 1;
+        N = N/10;
+
+    }
+
+    if (N > 0) {
+         printf("YEs");
+    } else {
+         printf("No");
+    }
+    return 0;
+}
