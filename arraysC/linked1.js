0f07b3ddf32e3a6d07fe1f2558bf3ff3a63db43c
mryan-3 <malawaryan@gmail.com>
2024-02-12 15:02:04 +0300

    Insert items at given indexes

--- arraysC/linked1.js
+++ arraysC/linked1.js
@@ -55,6 +55,25 @@ class LinkedList {
         }
         this.size++
     }
+    insert (value, index){
+        if (index < 0 || index > this.size){
+            return
+        }
+        if (index === 0){
+            this.prepend(value)
+        }else{
+            const node = new Node(value)
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++){
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+
+        }
+
+    }
 
 }
 
