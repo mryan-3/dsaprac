d1a6001e287cfb9842378051330104675e8b63a8
mryan-3 <malawaryan@gmail.com>
2024-02-12 12:52:08 +0300

    Append items

--- arraysC/linked1.js
+++ arraysC/linked1.js
@@ -19,7 +19,7 @@ class LinkedList {
         return this.size
     }
     prepend(value){
-        const node = Node(value)
+        const node = new Node(value)
         if(this.isEmpty()){
             this.head = node
         } else{
@@ -29,9 +29,41 @@ class LinkedList {
         this.size++
 
     }
+    print() {
+        if(this.isEmpty()){
+            console.log("List is empty")
+        } else{
+            let current = this.head
+            let listValues = ''
+            while(current){
+                listValues += `${current.value} `
+                current = current.next
+            }
+            console.log(listValues)
+        }
+    }
+    append(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.head = node
+        } else {
+            let prev = this.head
+            while(prev.next){
+                prev = prev.next
+            }
+            prev.next = node
+        }
+        this.size++
+    }
+
 }
 
 const list = new LinkedList()
+list.append(20)
+list.print()
 list.prepend(10)
+list.print()
 list.prepend(20)
+list.print()
 list.prepend(30)
+list.print()
