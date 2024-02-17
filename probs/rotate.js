a0a6492f57f3dce891f67e00103cbb5e658ee323
mryan-3 <malawaryan@gmail.com>
2024-02-17 15:53:24 +0300

    OPtimized approach to rotate an array without using built in methods

--- probs/rotate.js
+++ probs/rotate.js
@@ -20,3 +20,35 @@ const nums = [1,2,3,4, 5, 6 ,7]
 console.log(rotateArray(nums, 3))
 
 //Optimized solution Jones
+//[1, 2, 3,4 5, 6, ,7] >>>>>[7, 6, 5, 1, 2, ,3 , 4] >>>>> [5, 6, 7, ,4, 3, 2,1] >>> [5, 6, 7, 1, 2, 3, 4]
+function rotateArrayOptimised(nums, k){
+    let size = nums.length
+    if (k > size){
+        k = k% size
+    }
+    reverse(nums, 0, size -1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, size -1)
+
+    return nums
+
+}
+
+// [1, 2,3 ,4 ]
+// left = 0, right = 3
+// temp = 1
+// nums[0] = 4
+// nums[3] = 1
+// left = 1 right=2
+// [4 ,2, 3, 1]
+// temp = 2
+// nums[1]= 3
+// nums[2]= 2
+function reverse(nums, left, right){
+    while(left < right){
+        let temp = nums[left]
+        nums[left++] = nums[right]
+        nums[right--] = temp
+    }
+}
+
