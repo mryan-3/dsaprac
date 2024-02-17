9b65a590bfd5d67f46ab3b047d30debcc33cb1fe
mryan-3 <malawaryan@gmail.com>
2024-02-17 14:58:41 +0300

    Optimized approach for finding the second largest number

--- probs/secondMax.js
+++ probs/secondMax.js
@@ -5,13 +5,48 @@
 
 
 
-function secondLargest(arr){
-    arr.sort((a, b) => b-a)
-    const newArr = Array.from(new Set(arr))
+//function secondLargest(arr){
+    //arr.sort((a, b) => b-a) //has O(n log n) time complexity so its not the best
+    //const newArr = Array.from(new Set(arr))
 
-    return newArr[1]
+    //return newArr[1]
+//}
+
+
+//const arr = [1, 23, 54, 65, 7, 4, 5 , 89, 89, 1]
+//console.log(secondLargest(arr))
+
+
+// Optimised approach
+//big O of O(n)
+function secondLargestOp(arr){
+    //[1, 4, 6, 8, 9, 2, 5, 7]
+    //secondLargest = -1
+    //largest = 1
+    //secondLargest = -1
+    //largest = 4
+    //secondLargest = 4
+    //largest = 6
+    //secondLargest = 6
+    //largest = 8
+    //secondLargest = 8
+    //largest = 9
+    //secondLargest = 8
+    //
+    let largest = -1
+    let secondLargest = -1
+    for (let i = 0; i < arr.length ; i++){
+        if (arr[i] > largest){
+            secondLargest = largest
+            largest = arr[i]
+        } else if(arr[i] != largest && arr[i] > secondLargest){
+            secondLargest = arr[i]
+        }
+    }
+
+    return secondLargest
 }
 
+const arr = [1, 4, 6, 8, 9, 2, 5, 7]
+console.log(secondLargestOp(arr))
 
-const arr = [1, 23, 54, 65, 7, 4, 5 , 89, 89, 1]
-console.log(secondLargest(arr))
